384ac842c2903255dd7ea1c225c6421c1748172c
dillerm <dillerm@oregonstate.edu>
2016-10-02 20:46:23 -0700

    fixed paths to be relative

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f68e18e86d2817d1bf3db6933ecf165c01887e14b532204b9f04977cc25a5c2e
-size 14429986
+oid sha256:786016af761b53b0415a6eb2f442c7f52566d5fce3ec317fba007b28e7ee9ba0
+size 16099324
