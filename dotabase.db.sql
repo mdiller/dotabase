bb11fcb10caac7048d6e78c3de5e8872f4036348
dillerm <dillerm@oregonstate.edu>
2016-10-19 22:31:49 -0700

    updated to include simple_text

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:786016af761b53b0415a6eb2f442c7f52566d5fce3ec317fba007b28e7ee9ba0
-size 16099324
+oid sha256:c736fe10c74e1bc25a7e984f34d9b4784f98b8967b6c6dae676c03a4e5b8872d
+size 16833464
