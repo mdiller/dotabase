275475909e1ac9a8d2fcaf9925d98631c3e75c90
dillerm <dillerm@oregonstate.edu>
2016-10-26 21:42:16 -0700

    linked abilities to heroes

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d79f100a40c62e53141356d656ffccfe5862d719e5fdacebd83edbe00bed5f5
-size 8678103
+oid sha256:ef10fcd7db920c60162272413faf69ad923d78ddf4a5b2f88ff1db091ec0f471
+size 8682943
