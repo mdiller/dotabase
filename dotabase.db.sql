dd44db58483b7836b331a1f186d2358c366e9695
dillerm <dillerm@oregonstate.edu>
2017-02-02 19:25:32 -0800

    improved pretty criteria

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71755dd6a8e7b7e6fd8a4a8ee4d0f94c26987cf5e780986de45a012258fe33a6
-size 10730509
+oid sha256:20ccaa288c95abffe17c4c68b1563e463f2347e390ad85bd290739d1488ef005
+size 10713034
