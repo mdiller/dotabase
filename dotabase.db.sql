5ba3848ec33c6f81fa543b26174801b4e6343ab0
dillerm <dillerm@oregonstate.edu>
2017-03-13 23:05:55 -0700

    added emoticons

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1d71fb1c463a1963f9edd1a58d726cdc9cab07b4fabfec2a5f86099193e5c74
-size 10569178
+oid sha256:4c83d44a9e03bc9fad70a1983a3eebebe5e6915f937214645f4db8fe5dc12a13
+size 10586354
