8e409d1518c9d46ca7741ecde03c5f061997d667
dillerm <dillerm@oregonstate.edu>
2017-08-22 13:22:23 -0700

    updated setup script and moved files

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python3.5
+#!/usr/bin/env python3.6
 
 from distutils.core import setup
 
-setup(name='dotabase',
-      version='1.0',
-      description='Dota sqlite database access',
-      author='Malcolm Diller',
-      author_email='gward@python.net',
-      url='http://dotabase.me',
-      packages=['dotabase'],
-     )
+setup(
+	name='dotabase',
+	version='1.0',
+	description='Dota sqlite database access',
+	author='Malcolm Diller',
+	url='https://github.com/mdiller/dotabase',
+	packages=['dotabase'],
+)
