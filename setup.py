2f2e0409d67c4cc7988e7b475473423cf87ab22e
dillerm <dillerm@oregonstate.edu>
2017-08-22 14:13:14 -0700

    updated to dota 7.06f

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 	name='dotabase',
-	version='1.0',
+	version='1.1',
 	description='Dota sqlite database access',
 	author='Malcolm Diller',
 	url='https://github.com/mdiller/dotabase',
