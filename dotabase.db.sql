15b0a921a3e01d78d268bb1077fe51c1bb8b41ab
Malcolm Diller <dillerm@oregonstate.edu>
2017-11-01 20:36:34 -0700

    updated for more fixes after 7.07

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e51937a3e49d3e5c6ac232d2dfdd6c458c1aeaa7b0c4abc28a7f50e3e29ce84b
-size 11691822
+oid sha256:76ad7e168d5d8b6ecf92d380497455c77823c0a5dd505ef3a11cf15e90c85df6
+size 11701360
