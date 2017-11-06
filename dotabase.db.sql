d3a865b6c110f86ef80daac07b2e2a6c4c5d7e3a
dillerm <dillerm@oregonstate.edu>
2017-11-06 12:03:41 -0800

    added new responses text scraped from wiki (includes dark willow and pangolier)

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74c49ef57ff8b4886c622511d3833da35757635ddd4bfcef1d8d24c27e40bb3e
-size 11702031
+oid sha256:d2042d682bfa09f97096679f5c28cb6db628cf516a87fb6aebbbd6283b41c9f7
+size 11834083
