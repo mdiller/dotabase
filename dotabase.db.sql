a1daf3baf4f9af8263e9d9107ae2704ee27915ed
Malcolm Diller <dillerm@oregonstate.edu>
2018-01-29 13:27:35 -0800

    updated responses to include characters and other locations that were mistakenly removed

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f930decef2e345281a88462afcd98dae50a0cf0eaba7c3b82adf50230a195db9
-size 11896504
+oid sha256:1bd017d3ca48744fc7d3ce5b35b8bc2894ae4da82abd6630a68a8766f3b0e0dc
+size 11903991
