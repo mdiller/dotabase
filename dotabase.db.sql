e2738daa2398e9e1024024d71dcd9c223490a1a4
Malcolm Diller <dillerm@oregonstate.edu>
2018-01-29 17:30:06 -0800

    fixed up description tweaks again

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba298235c7066ad3b50c72a182ef22ba20b2ebad37a6278fe70ed664082ff8a0
-size 12304514
+oid sha256:5bbac37bdab8109716cecdfac80f15d609739452906fdb57dabbdb27f3582396
+size 12304278
