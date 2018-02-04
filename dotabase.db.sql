4cf944ace739e9aaedf5481636b4ffb8dd943ad4
Malcolm Diller <dillerm@oregonstate.edu>
2018-02-04 01:07:02 -0800

    fixed breaks in hero lore

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dba249d704c33d75881f441c16f8f2cc8b5c5aaaeef879942be1bb968db23a08
-size 12491753
+oid sha256:860ee0539341ba95b7fd66b706b63b6e6df3d117c60f7d4532ace23b72bf4630
+size 12491805
