67f61d4e97442effe6017379cddf743c23ae9461
Malcolm Diller <dillerm@oregonstate.edu>
2018-02-03 22:26:14 -0800

    fixed descriptions for necronomicon and dagon

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dec26182592691c00597ee300dce4425a01f5c281cd07c0998db7bc704c15be1
-size 12453010
+oid sha256:150b19465523f1a57c3cc50d65f6aeb29daf90229d514e1d486ad20c5fdc9fcf
+size 12453140
