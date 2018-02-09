38f2d757ddf4dc6ef48a7b48776649b8711b9d7e
Malcolm Diller <dillerm@oregonstate.edu>
2018-02-08 21:07:40 -0800

    added loadingscreens

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66144dfcae93d210aa63299fc2dd81dd94e3c0c9ec454f338191c3d3fabed4d6
-size 15638519
+oid sha256:f4ec626fb640b9ba61d5e03c372490851bdd9092498a59e282eff7a74acba69e
+size 15810311
