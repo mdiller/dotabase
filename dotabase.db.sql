67ddbf8474db4a59ed3a992948294ed790202eee
Malcolm Diller <dillerm@oregonstate.edu>
2018-02-16 16:56:09 -0800

    added announcer criteria

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19c1e42b9652ebad9467c8d653d8e9dcbf71dfe48a332bdd36fb5f913724aa20
-size 15810215
+oid sha256:2a9d9e32eb5356bac6d0fd2b781f5d4f85e3c340dcd14bf4e2f68a761dd41593
+size 17934750
