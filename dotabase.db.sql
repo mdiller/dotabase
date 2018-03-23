ed3c875adfcc209692da16b9cf5fdb46b71f58dc
Malcolm Diller <dillerm@oregonstate.edu>
2018-03-23 01:01:18 -0700

    added loadingscreen categories and switched to using a hero_ids column for them

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f20d4cd532e36c22fab71d786af76655ab01a742ace93f946fdcc99ef523fe09
-size 17445307
+oid sha256:13e5898ccf80e84c17943960bb29a98df2ebc5bbec0feae447d4f32e782568d8
+size 17453142
