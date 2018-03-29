28e307a0ddcbf73449dd8d55b8553b1562515dae
Malcolm Diller <dillerm@oregonstate.edu>
2018-03-29 02:31:26 -0700

    updated the prettifying of a lot of new kinds of criteria for responses

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:052cd95bbea6e24dae16d84b10b681f554a402d20ecfc14834528991ced69be4
-size 17318230
+oid sha256:97fa8165c07104006b6b7b2c8cbf647b69c27099cadbcbaa2b3778c1c8910c19
+size 17313330
