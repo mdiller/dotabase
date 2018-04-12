730b1a7abd01d021ee3896fdab790847704b4fab
Malcolm Diller <dillerm@oregonstate.edu>
2018-04-12 01:19:30 -0700

    updated setup.py and added to pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3.6
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+
+# Get the long description from the README file
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+	long_description = f.read()
 
 setup(
 	name='dotabase',
-	version='1.1',
-	description='Dota sqlite database access',
+	version='4.2.1',
+	description='Dota 2 game data extracted as an sqlite database, with an sqlalchemy wrapper',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
 	author='Malcolm Diller',
+	author_email='malcolm@dillerm.io',
 	url='https://github.com/mdiller/dotabase',
+	license='MIT',
+	keywords='dota dota2 data sqlite',
 	packages=['dotabase'],
+	install_requires=['sqlalchemy'],
 )
