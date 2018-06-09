da22a8a9d0d7875514b28ed80b78f37244503164
Malcolm Diller <dillerm@oregonstate.edu>
2018-06-08 19:55:23 -0700

    fixed wisp response files that are wavs instead of mp3s

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c4f18e2d7889c7939951d447c1bd07c7b72f89454bd3ba40801db35e1d25f04
+oid sha256:6ad833bdf8313f55a8695c59ea2081b4ab32d939db0fc899391b74ccfaf3abe0
 size 17405341
