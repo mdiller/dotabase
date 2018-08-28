ad0f06cd49d7294efd6c4f5aa177ca5d73ab529d
Malcolm Diller <dillerm@oregonstate.edu>
2018-08-27 21:49:30 -0700

    updated ability icons for grimstroke

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e85aa13fb93e473a7080519aae0d292da54842da56101ad92db0d5142e568cc
-size 17935710
+oid sha256:2ac1c8f7f220856f35a8e05448b50ed0379e58ce79d2cec2b8e9c1de4e55768c
+size 17935756
