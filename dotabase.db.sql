5bccd9596d5917c7d115f2ff593ea587170c4512
Malcolm Diller <dillerm@oregonstate.edu>
2018-09-18 01:13:30 -0700

    updated to dota 7.19c

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1605df8ade41b1a0593c3dad424c1229d93ad4b12d90f88aa1ebd00fe877e382
-size 17936492
+oid sha256:1741890dc260dc91fcfa30893f2792a0fd137cc23823eea319ce6caccec69110
+size 17936659
