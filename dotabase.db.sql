fe39306389bf2c9008510451fa140e5b5f822b0e
Malcolm Diller <malcolm@dillerm.io>
2019-03-19 02:19:33 -0700

    added arcana to voices list

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca858276a5fb2ab1a680a03cdd9fc02cf1506d452f478248b92b94f3914799af
-size 19338607
+oid sha256:2bfa6f46e8c5e9e4b23ac38198c4f7fcb682de36d0693ea2015460710cfad9c7
+size 19350490
