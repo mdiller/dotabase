3bd3126451a9ea4a158a42279e35870d2a0e3d7e
Malcolm Diller <malcolm@dillerm.io>
2019-05-08 19:00:03 -0700

    added changes from 2019 compendium

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6406dc5c9073c7d6d60f32dbec306a6556a8a4172c9ef06393f88c452181db9
-size 19349772
+oid sha256:4eeec628e53d6f8176f0a5c39a53dee6fa47d0fe7f4996874c1a4a7553f6c1b7
+size 19376716
