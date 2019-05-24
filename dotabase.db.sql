ef9c6beee4d9e8195b00e770aba62eea6835867a
Malcolm Diller <malcolm@dillerm.io>
2019-05-24 01:24:22 -0700

    added earthshaker arcana

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4eeec628e53d6f8176f0a5c39a53dee6fa47d0fe7f4996874c1a4a7553f6c1b7
-size 19376716
+oid sha256:1560412d66d1a19e4dacdc71fce34160e3703cba03f396eabca06d465e95871e
+size 19378856
