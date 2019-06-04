6cf8d9d1de5ed9816206521f88928aba942dfc55
Malcolm Diller <malcolm@dillerm.io>
2019-06-03 18:36:00 -0700

    fixed markdown parsing for pypi info

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os import path
 
 
 # Get the long description from the README file
-with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as f:
 	long_description = f.read()
 
 setup(
