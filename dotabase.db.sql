895e661753b74e1f28dbc82bd58e9ec622f7efbf
Malcolm Diller <malcolm@dillerm.io>
2020-02-19 21:45:07 -0800

    fixed to not include obsolete items

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d70455dfa689be0f923b176973aaf9bca4f44549560e5a786660b3f27fccabb
-size 21449781
+oid sha256:2d2ceeaa81cf7ecb77b6aab84d059f17cc846e4b471dd0e870d0a21c83846714
+size 21435795
