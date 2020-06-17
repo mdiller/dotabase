fa781d9b8e61b7257f365c47cc0b972a97aa76f0
Malcolm Diller <malcolm@dillerm.io>
2020-06-17 00:58:05 -0700

    updated w/ qop arcana

--- dotabase.db.sql
+++ dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3990680ee3ed296a5fa36ee21798cf3b5cef06956daadee204430fb61319b06
-size 21828716
+oid sha256:89c72e7c01809e31ee89ce9489936c2115acb01ef17bf450f8ad50fcfef302be
+size 22013592
