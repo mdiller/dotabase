d637ae42cb25f1ee82f39fedff3e2b13312334a6
Malcolm Diller <malcolm@dillerm.io>
2020-07-01 17:13:29 -0700

    did the actual update for patch 7.27

--- dotabase/dotabase.db.sql
+++ dotabase/dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4bcf67340fd3fb23b0476ed26eef00d276fcf6b5cca9be6c953b01a95089a4f
-size 22073040
+oid sha256:583deed3cf504f1c588803a3ba06065e1d3e59cfa7f855332fd9cc11dec41614
+size 22072123
