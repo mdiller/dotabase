671e9e0843e257e7bb0e2632c6f1064a16f926ae
Malcolm Diller <malcolm@dillerm.io>
2020-08-20 21:36:05 -0700

    greatly improved linkage and properties for talents

--- dotabase/dotabase.db.sql
+++ dotabase/dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2f31cdc43f74e4f7221fd25c9b3d4f489bf310f945ff065b3acde85e8491ce9
-size 22386024
+oid sha256:9a93129c867e801e84f3e9e36b3e17cf411501fe0e84adfa4f8211cfd8a0926b
+size 22414676
