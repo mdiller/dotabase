f4c8ff033bc8506ff4088cce11ddfc29b8fe92e3
Malcolm Diller <malcolm@dillerm.io>
2020-08-21 16:39:39 -0700

    added support for recipes

--- dotabase/dotabase.db.sql
+++ dotabase/dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a93129c867e801e84f3e9e36b3e17cf411501fe0e84adfa4f8211cfd8a0926b
-size 22414676
+oid sha256:05ca0a643b82e77f00d05f5aee8ca9aa2bbaf6eede6de67516f5d1b808f80f82
+size 22403932
