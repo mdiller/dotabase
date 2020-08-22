4a79ee4323264d0d757e0c56bb7d7c12d3b02aae
Malcolm Diller <malcolm@dillerm.io>
2020-08-21 17:19:43 -0700

    added update for toy butcher

--- dotabase/dotabase.db.sql
+++ dotabase/dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05ca0a643b82e77f00d05f5aee8ca9aa2bbaf6eede6de67516f5d1b808f80f82
-size 22403932
+oid sha256:bdccbf1c0c2efa97fbb8c954635c268913645f65540079cfb8cac2e6ec85b184
+size 22839448
