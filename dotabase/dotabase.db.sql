8fcd4b5f5fd595447da7de5e1f0171ee56cb9ae3
Malcolm Diller <malcolm@dillerm.io>
2020-10-01 19:55:20 -0700

    updated dotabase to include windranger arcana

--- dotabase/dotabase.db.sql
+++ dotabase/dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b3c1a0078a2505b84412e19def7df89573f534d06dc2ab3230272f7225de380
-size 22857223
+oid sha256:db08a5ad06ccfde656ae5fe62289a54ad949ea90ddcc6af1739aec2b99b0fbaf
+size 23215236
