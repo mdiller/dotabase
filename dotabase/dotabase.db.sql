4a6328f1f3a83f3d6c1d183722c44f06668ca7cd
Malcolm Diller <malcolm@dillerm.io>
2020-12-17 16:15:24 -0800

    updated to fix aghs information and add aghs shards information

--- dotabase/dotabase.db.sql
+++ dotabase/dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f50a5a83423ba28db6329b31e9bf5c51b368bea4b420dc16b748f8479d020e1b
-size 23963664
+oid sha256:46653ec00725f1df5fc5d851fe529bdea986336bbca8eab21fecc38fba0e41da
+size 23998240
