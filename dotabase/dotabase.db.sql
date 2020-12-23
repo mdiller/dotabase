f5389d016dc3433840359aa87c1535f00df30dd6
Malcolm Diller <malcolm@dillerm.io>
2020-12-22 17:37:39 -0800

    fixed aghs shard/scepter detection to actually use the right flags

--- dotabase/dotabase.db.sql
+++ dotabase/dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46653ec00725f1df5fc5d851fe529bdea986336bbca8eab21fecc38fba0e41da
-size 23998240
+oid sha256:d3b65273d1b7987102996190b971d780f163dc50b78f0704c0e25fae90c1b4b6
+size 23997585
