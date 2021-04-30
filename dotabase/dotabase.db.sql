a1c2160a26653fcc445e3efeced6ba1094b7ab80
Malcolm Diller <malcolm@dillerm.io>
2021-04-30 11:29:51 -0700

    updated to dota 7.29c

--- dotabase/dotabase.db.sql
+++ dotabase/dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb713f8532dedf92951992bd6a280722f24da36ff6d801a6feab3f5388a7a388
-size 24419708
+oid sha256:5acc89f77faa304bc9b49ea58c5536b699030cb0621717e726febb8551a40fa2
+size 24417611
