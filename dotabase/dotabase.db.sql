0c8fa0127f3d8810e9e1d8823aa9266fa1631a31
Malcolm Diller <malcolm@dillerm.io>
2021-12-16 18:01:27 -0800

    updated ability parsing to do shard upgrades better

--- dotabase/dotabase.db.sql
+++ dotabase/dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2952973a140ef0a056934c213a134134dd9877fb935986a3b2fffdd09b1c434
-size 26399293
+oid sha256:4fb0088877a7a56a80edf59b9d2d5d4a751249e6fc7fac026b0a39eb83a026d5
+size 26400270
