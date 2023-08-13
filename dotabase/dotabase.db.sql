e238942e6197543aff229e36fec47b902d68538e
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-08-13 09:45:07 +0500

    Revert "updated to 7.34"
    This reverts commit 492d7d58cf40d9ef4dbeaafd921e6d6bd357389e.

--- dotabase/dotabase.db.sql
+++ dotabase/dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4ec29f5779e09bf67e91bfe4f16805e2472b5b3371591d69f92a70b9a8ebabb
-size 58153572
+oid sha256:909763c35182eadfa64a97773259a2c9a454c1bee5b422f82625fe1f4cb1edef
+size 58856679
