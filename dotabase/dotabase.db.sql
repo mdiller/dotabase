18284baacf9bdab4885b683477667f37e4509a2f
Malcolm Diller <malcolm@dillerm.io>
2024-10-08 18:09:03 -0700

    updated to 7.37d, and fixed a bunch of issues with facets n talents n stuff

--- dotabase/dotabase.db.sql
+++ dotabase/dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd20dde47f8d25d26131282bff3d3f1bd86d54d197c35e3b2d8504bbc098d871
-size 64349968
+oid sha256:43aafab0b8dc0017546bb232ed32f66942e5e096bed52bb7f7df6b4bdde895f9
+size 64687624
