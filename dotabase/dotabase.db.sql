63520cca4ef7b0713dae91f5e4d096a4d4b94222
Malcolm Diller <malcolm@dillerm.io>
2025-06-09 19:06:21 -0700

    fixed the facet bugs hopefully

--- dotabase/dotabase.db.sql
+++ dotabase/dotabase.db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bda625bfa01e34bc81d06843269e1d8ab4425f3b465df6009e53869956ac84fd
-size 67640078
+oid sha256:4ea01d81b27c9b18358be6d974555c2ba0beb29c6c19fecd2b10320906e9cd20
+size 67640231
